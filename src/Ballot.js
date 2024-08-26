124a0b4c078d90a2c00769aec2790994ad4f4d22
stepandel <stepandel@gmail.com>
2024-08-26 17:19:29 -0400

    support for round 5 ballot

--- src/Ballot.js
+++ src/Ballot.js
@@ -5,27 +5,47 @@ export function Ballot() {
   const signer = useSignMessage();
   const { address } = useAccount();
 
-  const ballot_content = {
+  const r4_ballot_content = {
     allocations: [{ gas_fees: 100 }],
     os_only: false,
     os_multiplier: 1,
+  };
+
+  const r5_ballot_content = {
+    project_allocations: [
+      {
+        1: 100,
+      },
+    ],
+    category_allocations: [
+      {
+        FOO_BAR: 100,
+      },
+    ],
   };
 
   return (
     <StyledButton
       onClick={async () => {
         const signature = await signer.signMessageAsync({
-          message: JSON.stringify(ballot_content),
+          message: JSON.stringify(r5_ballot_content),
         });
 
         console.log(signature);
 
-        fetch(`api/v1/retrofunding/rounds/4/ballots/${address}/submit`, {
+        const accessToken = localStorage.getItem("accessToken");
+
+        fetch(`api/v1/retrofunding/rounds/5/ballots/${address}/submit`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
           },
-          body: JSON.stringify({ signature, address, ballot_content }),
+          body: JSON.stringify({
+            signature,
+            address,
+            ballot_content: r5_ballot_content,
+          }),
         });
       }}
     >
